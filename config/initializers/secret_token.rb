bbabab656f3368865c85f96ffe6eaeca62a39769
songqin <richieqin@gmail.com>
2014-01-02 21:35:34 -0500

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'df1fe97455510a23425ac435f3534830346cceb7488d3f0a144cd0a42d99b2c7cf9bdcf3f9fc3b3521d7f8d013df25cf3e9766a2f3f0161c5e0db9817f784cb1'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
